837bc744255e543a6e82889e19238089a8b1aa5e
shayanealcantara <shayaneliebe@gmail.com>
2018-04-21 20:07:42 -0300

    Adding library and comments

--- funcoes.c
+++ funcoes.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 selectImage()
 int imagem;
 
 imagem = (rand()%25)+1;
+
+for (i = 0; i < 25; i++)
+     {
+  /* gerando valores aleatórios entre zero e 35
+  printf("%d ", rand() % 25);
+} */
+
   return 0;
 }
