7d480fa937f417c847730d95b95fbda020dd6e4f
shayanealcantara <shayaneliebe@gmail.com>
2018-04-29 20:50:10 -0300

    Adding condition fp NULL

--- funcoes.c
+++ funcoes.c
@@ -8,9 +8,18 @@ imagem = (rand()%25)+1;
 
 for (i = 0; i < 25; i++)
      {
-  /* gerando valores aleatorios entre zero e 35
+  /* gerando valores aleatorios entre zero e 25
   printf("%d ", rand() % 25);
-} */
+ */
+}
+
+
+
+
+
+if (fp==NULL) {
+printf("Falha.\n");
+exit(1);
 
   return 0;
 }
