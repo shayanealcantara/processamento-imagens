34efd65d9d2d09602c2be982acecdf61f96773ae
Shayane <shayaneliebe@gmail.com>
2018-04-30 15:34:00 -0300

    Adding conversion binary to decimal

--- funcoes.c
+++ funcoes.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <math.h>
+#include <curses.h> //binaryToDecimal add -lncurses to the compile.
 
 void selectImage(){
 	int image[25];
 
-	srand(time(NULL)); // gerando valores aleatorios entre zero e 25
+	srand(time(NULL)); // generating random values between 0 and 25
 	for(int i = 0; i < 25; i++){
 		image[i]= (rand()%25)+1;
 		printf("%d ", image[i]);
@@ -21,7 +23,29 @@ void readImage(){
   if (fp==NULL) {
   printf("Falha.\n");
   exit(1);
+  }
 }
+
+/*void avarageMatrix(*int number, *int MAX){
+  int number[], sum;
+  float avarage;
+
+  sum = number[]+= sum;
+  avarage = (sum)/9.0;
+
+}*/
+
+void binaryToDecimal(*int mBinary){
+  int i, binary[4];
+  float decimal = 0;
+    for(i = 0; binary > 4; i++){
+        decimal = decimal + pow(2, i) * (binary % 10);
+        binary = binary / 10;
+
+        return decimal;
+    }
+}
+
 
 int main(){
 	selectImage();
